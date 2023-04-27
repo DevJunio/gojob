42d3de84463e34df8b09bdb5d97917d5c5ed8d18
Junio Koi <53125029+Juniokoi@users.noreply.github.com>
2023-04-27 13:47:17 -0300

    chore: setup main route

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"gojob/config"
 	"gojob/router"
 )
 
-func main()  {
+var (
+	logger *config.Logger
+)
+
+func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize the config
+	err := config.Init()
+	if err == nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
-
